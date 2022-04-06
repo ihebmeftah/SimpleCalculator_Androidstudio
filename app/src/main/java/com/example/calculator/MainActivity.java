1992303a79b5fc52ed8d48be211cf8d5f4ff3003
iheb <salimbb041@gmail.com>
2022-04-07 00:22:16 +0100

    TextUtils

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.calculator;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,18 +14,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText input1 ,input2;
+    EditText input1, input2;
     Button mytBtn;
     TextView myResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         input1 = findViewById(R.id.number1);
-         input2 = findViewById(R.id.number2);
-         mytBtn = findViewById(R.id.sum);
-         myResult = findViewById(R.id.result) ;
+        input1 = findViewById(R.id.number1);
+        input2 = findViewById(R.id.number2);
+        mytBtn = findViewById(R.id.sum);
+        myResult = findViewById(R.id.result);
 
 
         mytBtn.setOnClickListener(new View.OnClickListener() {
@@ -31,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View view) {
-                double n1 = Double.parseDouble(input1.getText().toString());
-                double n2 = Double.parseDouble(input2.getText().toString());
-                myResult.setText("Result " + (n1+n2));
-                myResult.setTextColor(getColor(R.color.purple_700));
+                if (TextUtils.isDigitsOnly(input1.getText().toString())
+                        && TextUtils.isDigitsOnly(input1.getText().toString())
+                ) {
+                    double n1 = Double.parseDouble(input1.getText().toString());
+                    double n2 = Double.parseDouble(input2.getText().toString());
+                    myResult.setText("Result " + (n1 + n2));
+                    myResult.setTextColor(getColor(R.color.purple_700));
 
-               Toast.makeText(getApplicationContext(), "operation done", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "operation done", Toast.LENGTH_LONG).show();
+                }else {
+
+                    myResult.setTextColor(Color.RED);
+                    myResult.setText("please enter a number");
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+
 
             }
         });
